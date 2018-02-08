f332975ac11d85533b6f518c52bb7be5744c60fa
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2018-02-08 15:38:09 +0100

    Revert "dir.c: stop ignoring opendir() error in open_cached_dir()"
    This reverts commit b6731550749c7326f0dcf26211e7c2e55de00e28.

--- dir.c
+++ dir.c
@@ -1857,16 +1857,11 @@ static int open_cached_dir(struct cached_dir *cdir,
 			   struct strbuf *path,
 			   int check_only)
 {
-	const char *c_path;
-
 	memset(cdir, 0, sizeof(*cdir));
 	cdir->untracked = untracked;
 	if (valid_cached_dir(dir, untracked, istate, path, check_only))
 		return 0;
-	c_path = path->len ? path->buf : ".";
-	cdir->fdir = opendir(c_path);
-	if (!cdir->fdir)
-		warning_errno(_("could not open directory '%s'"), c_path);
+	cdir->fdir = opendir(path->len ? path->buf : ".");
 	if (dir->untracked) {
 		invalidate_directory(dir->untracked, untracked);
 		dir->untracked->dir_opened++;
