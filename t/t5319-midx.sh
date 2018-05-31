31a2031e1572ebfd10532e7661b76e3fd6f2d308
Derrick Stolee <dstolee@microsoft.com>
2018-05-31 08:36:23 -0400

    midx: write header information to lockfile
    As we begin writing the multi-pack-index format to disk, start with
    the basics: the 12-byte header and the 20-byte checksum footer. Start
    with these basics so we can add the rest of the format in small
    increments.
    
    As we implement the format, we will use a technique to check that our
    computed offsets within the multi-pack-index file match what we are
    actually writing. Each method that writes to the hashfile will return
    the number of bytes written, and we will track that those values match
    our expectations.
    
    Currently, write_midx_header() returns 12, but is not checked. We will
    check the return value in a later commit.
    
    Signed-off-by: Derrick Stolee <dstolee@microsoft.com>

--- t/t5319-midx.sh
+++ t/t5319-midx.sh
@@ -3,8 +3,9 @@
 test_description='multi-pack-indexes'
 . ./test-lib.sh
 
-test_expect_success 'write midx with no pakcs' '
-	git midx --object-dir=. write
+test_expect_success 'write midx with no packs' '
+	git midx --object-dir=. write &&
+	test_path_is_file pack/multi-pack-index
 '
 
 test_done
